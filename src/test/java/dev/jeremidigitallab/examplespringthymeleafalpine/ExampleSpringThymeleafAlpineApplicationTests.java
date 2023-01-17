43693c29a8e75a9eedb0ce5f8bd056c7bd7bc87f
Jeremiah Hasudungan <jeremiah.sihombing97@gmail.com>
2023-01-17 16:00:44 +0700

    refactor : package and file structure

--- src/test/java/dev/jeremidigitallab/examplespringthymeleafalpine/ExampleSpringThymeleafAlpineApplicationTests.java
+++ src/test/java/dev/jeremidigitallab/examplespringthymeleafalpine/ExampleSpringThymeleafAlpineApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.jeremidigitallab.learn;
+package dev.jeremidigitallab.examplespringthymeleafalpine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
